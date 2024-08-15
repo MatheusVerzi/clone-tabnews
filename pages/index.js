087d5d9a2d0fdfbf015f10a19eae650dfb54c1d0
MatheusVerzi <63725551+MatheusVerzi@users.noreply.github.com>
2024-08-15 15:48:32 +0000

    Adicionar pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Estou aprendendo Front End. Quero ganhar mais dinheiro como desenvolverdor Full Stack e sair da casa da minha SOGRA... Por favor me ajude dando o LIKE</h1>
+}
+
+export default Home;
