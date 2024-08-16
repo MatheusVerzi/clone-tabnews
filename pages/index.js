e9eafa1094a350071ab090a348ffa1dfcca803f0
verzi.matheus <verzi.matheus@outlook.com>
2024-08-16 20:34:02 -0300

    adicionado virgula

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Estou aprendendo Front End. Quero ganhar mais dinheiro como desenvolverdor Full Stack e sair da casa da minha SOGRA... Por favor me ajude dando o LIKE</h1>
+    return <h1>Estou aprendendo Front End. Quero ganhar mais dinheiro como desenvolverdor Full Stack, e sair da casa da minha SOGRA... Por favor me ajude dando o LIKE</h1>
 }
 
 export default Home;
